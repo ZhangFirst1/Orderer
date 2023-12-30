2a7e87a814ecf33fb5f10dfcf45755cac139f7be
ZhangFirst1 <2246707693@qq.com>
2023-12-30 11:30:05 +0800

    zy 12.30

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "tcpclient.h"
 #include "tcpservers.h"
 #include "login.h"
+#include "adminlogin.h"
 
 #include <QApplication>
-#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     //MainWindow w;
     //w.show();
     Login l;
